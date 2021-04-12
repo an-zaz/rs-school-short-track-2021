5085674b574b4edb516d2cb361b311735a551a96
an-zaz <an_zazerskaya@mail.ru>
2021-04-12 18:35:54 +0300

    fix: fix eslint problems

--- src/01-matrix-elements-sum.js
+++ src/01-matrix-elements-sum.js
@@ -18,10 +18,9 @@ function getMatrixElementsSum(matrix) {
   let sum = 0;
   for (let i = 0; i < matrix.length; i++) {
     for (let j = 0; j < matrix[i].length; j++) {
-      if (i !== 0 && matrix[i - 1][j] === 0) {
-        continue;
+      if (i === 0 || matrix[i - 1][j] !== 0) {
+        sum += matrix[i][j];
       }
-      sum += matrix[i][j];
     }
   }
   return sum;
