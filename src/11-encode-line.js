839dbffdcec03a1e9884051fe0997d7f1a4af59e
an-zaz <an_zazerskaya@mail.ru>
2021-04-13 01:22:15 +0300

    feat: finish encode-line task

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,18 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  let count = 1;
+  let newStr = '';
+  for (let i = 0; i < str.length; i ++) {
+    if (str[i] === str[i + 1]) {
+      count++;
+    } else {
+      newStr += count > 1 ? `${count}${str[i]}` : str[i];
+      count = 1;
+    }
+  }
+  return newStr;
 }
 
 module.exports = encodeLine;
